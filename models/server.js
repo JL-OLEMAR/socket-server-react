d45a091b058c80f55a07159d5c656cec50963b5e
JL-OLEMAR <joseluis19963@gmail.com>
2021-02-27 00:27:46 -0500

    io

--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const path = require('path')
+const cors = require('cors')
 const Sockets = require('./sockets')
 
 class Server {
@@ -19,6 +20,9 @@ class Server {
   middlewares () {
     // Desplegar el directorio publico
     this.app.use(express.static( path.resolve(__dirname, '../public') )) // eslint-disable-line
+
+    // CORS
+    this.app.use(cors())
   }
 
   configurarSockets () {
