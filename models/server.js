b68c532b7e0669b535fdac73105c792e12e3c13f
JL-OLEMAR <joseluis19963@gmail.com>
2021-02-26 20:05:40 -0500

    Primer commit

---
+++ models/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const path = require('path')
+const Sockets = require('./sockets')
+
+class Server {
+  constructor () {
+    this.app = express()
+    this.port = process.env.PORT
+
+    // Http server
+    this.server = http.createServer(this.app)
+
+    // Configuracion del socket server
+    this.io = socketio(this.server, {/* configuraciones */})
+  }
+
+  middlewares () {
+    // Desplegar el directorio publico
+    this.app.use(express.static( path.resolve(__dirname, '../public') )) // eslint-disable-line
+  }
+
+  configurarSockets () {
+    new Sockets(this.io) // eslint-disable-line
+  }
+
+  execute () {
+    // Inicializar Middlewares
+    this.middlewares()
+
+    // Inicializar sockets
+    this.configurarSockets()
+
+    // Inicializar Server
+    this.server.listen(this.port, () => {
+      console.log(`Server corriendo en: http://localhost:${this.port}/`)
+    })
+  }
+}
+
+module.exports = Server
