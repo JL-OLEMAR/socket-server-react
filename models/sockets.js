54527466cd9cdb4bf9ee803f9ac208e1ef4f71c0
JL-OLEMAR <joseluis19963@gmail.com>
2021-03-03 02:26:31 -0500

    bands

--- models/sockets.js
+++ models/sockets.js
@@ -1,17 +1,19 @@
+const BandList = require('./band-list')
+
 class Sockets {
   constructor (io) {
     this.io = io
+    this.bandList = new BandList()
     this.socketEvents()
   }
 
   socketEvents () {
     // On connection
     this.io.on('connection', (socket) => {
-      // Escuchar evento: mensaje-to-server
-      socket.on('mensaje-to-server', (data) => {
-        console.log(data)
-        this.io.emit('mensaje-from-server', data)
-      })
+      console.log('Cliente conectado')
+
+      // Emitir al cliente conectado, todas las bandas actuales
+      socket.emit('current-bands', this.bandList.getBands())
     })
   }
 }
