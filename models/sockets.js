960472bffde274d2968e47e9745f243b92202956
JL-OLEMAR <joseluis19963@gmail.com>
2021-03-08 06:37:23 -0500

    implementacion de docker

--- models/sockets.js
+++ models/sockets.js
@@ -14,6 +14,30 @@ class Sockets {
 
       // Emitir al cliente conectado, todas las bandas actuales
       socket.emit('current-bands', this.bandList.getBands())
+
+      // votar por la banda
+      socket.on('votar-banda', (id) => {
+        this.bandList.increaseVotes(id)
+        this.io.emit('current-bands', this.bandList.getBands())
+      })
+
+      // Borrar banda
+      socket.on('borrar-banda', (id) => {
+        this.bandList.removeBand(id)
+        this.io.emit('current-bands', this.bandList.getBands())
+      })
+
+      // Cambiar nombre de la banda
+      socket.on('cambiar-nombre-banda', ({ id, nombre }) => {
+        this.bandList.changeName(id, nombre)
+        this.io.emit('current-bands', this.bandList.getBands())
+      })
+
+      // Crear una nueva banda
+      socket.on('crear-banda', ({ nombre }) => {
+        this.bandList.addBand(nombre)
+        this.io.emit('current-bands', this.bandList.getBands())
+      })
     })
   }
 }
